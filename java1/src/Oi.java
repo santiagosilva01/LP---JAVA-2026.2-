18291d2b2a07c636469dbf772c67d87e8aa0facb
jose.santiago <jose.santiago@dcx.ufpb.br>
2026-09-01 14:49:47 -0300

    repetição

--- java1/src/Oi.java
+++ java1/src/Oi.java
@@ -1,7 +1,7 @@
-public class repeticao {
+public class Oi {
 
 
-    public static void main() {
+    public static void main(String[] args) {
         for(int i = 1; i <=10; i++){
             System.out.println(i + " oi");
 
